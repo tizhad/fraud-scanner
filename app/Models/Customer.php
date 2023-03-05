6795db9ab82612720a1aac62fd8e8acc8a25f692
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-06 00:06:03 +0100

    Edit fillable based on customer data.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Model\Scan;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['customer_id', 'date_of_birth','phone_number','iban','is_fraud','ip_address'];
+
     public function scan(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Scan::class);
