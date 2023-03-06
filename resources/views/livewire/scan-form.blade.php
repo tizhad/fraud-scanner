af7abe36d9027806916f06b68de75c10ec796179
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-06 09:31:39 +0100

    Edit table to show a proper UI.

--- resources/views/livewire/scan-form.blade.php
+++ resources/views/livewire/scan-form.blade.php
@@ -3,30 +3,32 @@
         <x-primary-button wire:click="onScanStart">Start Scan</x-primary-button>
     </div>
     @if($scan)
-        <table class=" table-auto">
-            <thead>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th>ID</th>
-                <th>IBAN</th>
-                <th>IP</th>
-                <th>Fraud?</th>
-                <th>Customer ID</th>
-                <th>Created At</th>
+                <th  scope="col" class="px-6 py-3">ID</th>
+                <th  scope="col" class="px-6 py-3">IBAN</th>
+                <th  scope="col" class="px-6 py-3">IP</th>
+                <th  scope="col" class="px-6 py-3">Fraud?</th>
+                <th  scope="col" class="px-6 py-3">Customer ID</th>
+                <th  scope="col" class="px-6 py-3">Created At</th>
             </tr>
             </thead>
             <tbody>
             @foreach($scan->customers as $customer)
-                <tr>
-                    <td>{{$customer->id}}</td>
-                    <td>{{$customer->iban}}</td>
-                    <td>{{$customer->ip_address}}</td>
-                    <td>{{$customer->is_fraud}}</td>
-                    <td>{{$customer->customer_id}}</td>
-                    <td>{{$customer->created_at}}</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$customer->id}}</th>
+                    <td class="px-6 py-4">{{$customer->iban}}</td>
+                    <td class="px-6 py-4">{{$customer->ip_address}}</td>
+                    <td class="px-6 py-4">{{$customer->is_fraud}}</td>
+                    <td class="px-6 py-4">{{$customer->customer_id}}</td>
+                    <td class="px-6 py-4">{{$customer->created_at}}</td>
                 </tr>
             @endforeach
             </tbody>
         </table>
+        </div>
 
     @endif
 
