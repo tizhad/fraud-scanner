fb3045d0a2f0a870609d6475159f9b0394deed22
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-06 21:18:26 +0100

    Edit migration to show the fraud reason.

--- database/migrations/2023_03_04_210336_create_customers_table.php
+++ database/migrations/2023_03_04_210336_create_customers_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,10 +19,12 @@ return new class extends Migration
             $table->string('iban');
             $table->timestamps();
             $table->boolean('is_fraud');
+            $table->string('fraud_reason')->nullable();
             $table->unsignedBigInteger('scan_id');
             $table->foreign('scan_id')->references('id')->on('scans');
         });
     }
+
     /**
      * Reverse the migrations.
      */
